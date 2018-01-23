f2bee452b390aa86ab6b41f71142ecb6991b6c4e
Martin Eneqvist <marlun78@hotmail.com>
2018-01-23 17:58:18 +0100

    Fixing tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
 			},
 			resolve: {
 				alias: {
-					'preact-router': __dirname+'/src/index.js',
+					'@betit/preact-router': __dirname+'/src/index.js',
 					src: __dirname+'/src'
 				}
 			}
