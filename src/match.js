ea70d1eddb13591e9e7f8759d668144a6466cd44
Martin Eneqvist <marlun78@hotmail.com>
2018-01-23 16:53:39 +0100

    Updated internal import path

--- src/match.js
+++ src/match.js
@@ -1,5 +1,5 @@
 import { h, Component } from 'preact';
-import { subscribers, getCurrentUrl, Link as StaticLink } from 'preact-router';
+import { subscribers, getCurrentUrl, Link as StaticLink } from '@betit/preact-router';
 
 export class Match extends Component {
 	update = url => {
